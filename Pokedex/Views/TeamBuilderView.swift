b22372861eeea6e0e900e85d9f6dff09f16bc74a
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-12-03 16:45:07 -0600

    Creation of branch, created a placeholder for various elements

--- Pokedex/Views/TeamBuilderView.swift
+++ Pokedex/Views/TeamBuilderView.swift
@@ -9,12 +9,59 @@ import SwiftUI
 
 struct TeamBuilderView: View {
     var body: some View {
-        Text("TeamBuilder View")
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            VStack {
+                Image("teamBuilder")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .padding()
+                
+                teamOptions()
+                    .padding(.vertical,30)
+                Button(action: {}) {
+                    Text("RANDOMIZE")
+                        .font(.system(size: 25))
+                        .fontWeight(.semibold)
+                        .lineLimit(nil)
+                        .background(RoundedRectangle(cornerRadius: 10)
+                                        .strokeBorder(.black, lineWidth: 2)
+                                        .background(Color("PokemonRed"))
+                                        .frame(width: 200, height: 40, alignment: .center))
+                                  
+                                        
+                        .foregroundColor(.white)
+                        
+                }
+                .padding(.bottom,300)
+            }
+        }
     }
 }
+
+
+struct teamOptions: View{
+    var body: some View{
+        VStack{
+            HStack{
+                Text("hola")
+                Text("hola")
+                Text("hola")
+            }
+            HStack{
+                Text("hola")
+                Text("hola")
+                Text("hola")
+            }
+        }
+    }
+}
+
 
 struct TeamBuilderView_Previews: PreviewProvider {
     static var previews: some View {
         TeamBuilderView()
     }
 }
+
